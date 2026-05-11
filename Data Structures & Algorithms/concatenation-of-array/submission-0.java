db314f91afc6af3f8e63b5ecfadf0c8e2c36f1f7
Arjun Prithvi V <93268508+arjunprithvi3@users.noreply.github.com>
2026-05-11 14:00:03 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+
+        int n = nums.length;
+        int output[] = new int[n+n];
+
+        for(int i=0;i<n;i++)
+        {
+            output[i] = nums[i];
+            output[i+n] = nums[i];
+        }
+        return output;
+    }
+   
+}
