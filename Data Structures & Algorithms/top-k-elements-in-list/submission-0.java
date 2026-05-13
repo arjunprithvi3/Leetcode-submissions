74a79ef2f69d26367e0cd80f8ea0cc6153ed6268
Arjun Prithvi V <93268508+arjunprithvi3@users.noreply.github.com>
2026-05-13 15:41:50 +0530

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+
+        
+        int n = nums.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int x : nums)
+        {
+            map.put(x,map.getOrDefault(x,0)+1);
+        }
+
+        List<Map.Entry<Integer,Integer>> list = new ArrayList<>(map.entrySet());
+
+        list.sort((a,b) -> b.getValue() - a.getValue());
+
+        int[] res = new int[k];
+        for(int i=0;i<k;i++)
+        {
+            res[i] = list.get(i).getKey();
+        }
+
+        return res;
+        
+    }
+}
